9e0e7b1d70d14eaa18e8a419b48a0e9e8785fd00
niharikav27 <neerjav80@gmail.com>
2026-03-19 23:04:01 +0530

    initial commit

---
+++ ARRAY/linear_search.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int linearsearch(int arr[] , int n , int num){
+    for(int i = 0 ; i<n ; i++){
+        if(arr[i] == num){
+            return i;
+        }
+    }
+    return -1;
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    int num;
+    cin>>num;
+    int ans = linearsearch(arr,n,num);
+
+    cout<<ans;
+}
