109c82d71e4af6d5e24b8eaf8d9b1c1e2b6e9a01
niharikav27 <neerjav80@gmail.com>
2026-03-22 10:58:53 +0530

    optimized

---
+++ ARRAY/longest_subarray_positive1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    vector<int> a(n);
+    for(int i = 0 ; i<n ; i++){
+        cin>>a[i];
+    }
+    int k;
+    cin>>k;
+    map<long long ,int>preSumMap;
+    long long sum = 0;
+    int maxlen = 0;
+    for(int i = 0 ; i<a.size() ; i++){
+        sum += a[i];
+        if(sum == k){
+            maxlen = max(maxlen , i+1);
+        }
+        int rem = sum - k;
+        if(preSumMap.find(rem) != preSumMap.end()){
+            int len = i - preSumMap[rem];
+            maxlen = max(maxlen , len);
+        }
+        preSumMap[sum] = i;
+    }
+    cout<<maxlen;
+
+}
