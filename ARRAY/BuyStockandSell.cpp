2fb4f6242fd6a74901b050e503d088c33487f4e4
niharikav27 <neerjav80@gmail.com>
2026-03-27 11:53:07 +0530

    initial commit

---
+++ ARRAY/BuyStockandSell.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    vector<int> a(n);
+    for(int i = 0 ; i<n ; i++){
+        cin>>a[i];
+    }
+    int mini = a[0];
+    int profit = 0;
+    for(int i = 0 ; i<n ; i++){
+        int cost = a[i] - mini ;
+        profit = max(profit , cost);
+        mini = min(mini , a[i]);
+    }
+    cout<<profit;
+}    
