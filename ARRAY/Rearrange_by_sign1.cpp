2e2e3d189215e8824da114cf8e6edaa5e442e582
niharikav27 <neerjav80@gmail.com>
2026-03-29 09:31:07 +0530

    optimal approach, pos!= neg

---
+++ ARRAY/Rearrange_by_sign1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    vector<int>arr(n);
+    for(int  i = 0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    vector<int>pos, neg;
+    for(int i = 0 ; i<n ; i++){
+        if(arr[i]>0){
+            pos.push_back(arr[i]);
+        }else{
+            neg.push_back(arr[i]);
+        }
+    }
+    if(pos.size() > neg.size()){
+        for(int i = 0 ; i<neg.size() ; i++){
+            arr[2*i] = pos[i];
+            arr[2*i +1] = neg[i];
+        }
+        int index = neg.size()*2;
+        for(int i = neg.size() ; i<pos.size() ; i++){
+            arr[index] = pos[i];
+            index++;
+        }   
+    }else{
+        for(int i = 0 ; i<pos.size() ; i++){
+            arr[2*i] = pos[i];
+            arr[2*i +1] = neg[i];
+        }
+        int index = pos.size()*2;
+        for(int i = pos.size() ; i<neg.size() ; i++){
+            arr[index] = neg[i];
+            index++;
+        } 
+    }
+    for(int i = 0 ; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+}
