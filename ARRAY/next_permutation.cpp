7f9a636a58d901d6b8ee24210675365e3a276261
niharikav27 <neerjav80@gmail.com>
2026-03-29 09:44:16 +0530

    better sol using STL

---
+++ ARRAY/next_permutation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+
+    next_permutation(nums.begin(), nums.end());
+
+    cout << "Next Permutation: ";
+    for(int x : nums) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
