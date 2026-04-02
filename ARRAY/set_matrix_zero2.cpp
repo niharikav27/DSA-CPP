5c63f3826aa11e7302e2c77f91426db6123c76b5
niharikav27 <neerjav80@gmail.com>
2026-04-02 08:04:20 +0530

    optimal solution

---
+++ ARRAY/set_matrix_zero2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> matrix(n, vector<int>(m));
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int col0 = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(matrix[i][j] == 0){
+                //mark row
+                matrix[i][0] = 0;
+                //mark column
+                if(j != 0){
+                    matrix[0][j] = 0;
+                }else{
+                    col0 = 0;
+                }
+            }
+        }
+    }
+
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < m; j++) {
+            if(matrix[i][j] != 0){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    if(matrix[0][0] = 0){
+        for(int j = 0 ; j<m ; j++){
+            matrix[0][j] = 0;
+        }
+    }
+
+    if(col0 == 0){
+        for(int i = 0 ; i<n ; i++){
+            matrix[i][0] = 0;
+        }
+    }
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
