8a3c3c3f52964e09eda768fe7d526e916372dc55
niharikav27 <neerjav80@gmail.com>
2026-04-04 08:06:16 +0530

    brute force

---
+++ ARRAY/total_subarrays.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin >>target;
+    int count = 0;
+    for(int i = 0 ; i<n ;i++){
+        for(int j = i ; j<n ; j++){
+            int sum = 0;
+            for(int k = i ; k<=j ; k++){
+                sum += arr[k];
+            }
+            if(sum == target){
+                    count++;
+            }
+        }
+    }
+    cout<<count;
+    return 0 ;
+}    
