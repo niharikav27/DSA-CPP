e708a3225b07e3cc5235ce219c1bb22c520539d9
niharikav27 <neerjav80@gmail.com>
2026-04-16 09:56:41 +0530

    brute force

---
+++ ARRAY/subarray_with_xor_k.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraysWithXorK(vector<int> &arr, int k) {
+    int n = arr.size();
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+
+            int xr = 0;
+
+            for (int t = i; t <= j; t++) {
+                xr = xr ^ arr[t];
+            }
+
+            if (xr == k) {
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int k;
+    cin >> k;
+
+    int result = subarraysWithXorK(arr, k);
+    cout << result;
+
+    return 0;
+}
