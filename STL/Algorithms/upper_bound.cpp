d40333f0fa3c53668e0bbc01e0df2f8793ba1fce
niharikav27 <neerjav80@gmail.com>
2026-04-24 09:36:16 +0530

    initial commit

---
+++ STL/Algorithms/upper_bound.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int x;
+    cin >> x;
+
+    auto it = upper_bound(arr.begin(), arr.end(), x);
+    cout << (it - arr.begin());
+}  
