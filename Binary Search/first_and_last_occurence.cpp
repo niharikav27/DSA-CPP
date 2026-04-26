81b71988038b38fcb7b4b9d4da0390426a548830
niharikav27 <neerjav80@gmail.com>
2026-04-26 09:26:40 +0530

    brute force - linear search

---
+++ Binary Search/first_and_last_occurence.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i = 0 ; i<n ;i++){
+        cin>>arr[i];
+    }
+    int x;
+    cin>>x;
+
+   int first = -1 , last = -1;
+   for(int i = 0 ; i<n ;i++){
+    if(arr[i] == x){
+        if(first == -1) first = i;
+        last = i;
+    }
+   }
+   cout<<"first : "<<first<<endl;
+   cout<<"last : "<<last;
+   return 0;
+}
