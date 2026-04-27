a527dcfe89365f8b77d0c6fd174828fcf17672b7
niharikav27 <neerjav80@gmail.com>
2026-04-27 12:17:16 +0530

    array having unique elements

---
+++ Binary Search/rotated_sorted_array1.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(int n, vector<int> &arr, int target){
+    int low = 0,high = n-1;
+    while(low <= high){
+        int mid = (low + high)/2;
+
+        if(arr[mid] == target)return mid;
+        if(arr[low] <= arr[mid]){
+            if(arr[low] <= target && target <= arr[mid]){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }else{
+            if(arr[mid] <= target && target <= arr[high]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+
+    int ans = search(n,arr,target);
+    cout << ans;
+}    
