f0cf3779eca7da39d3733d1d3ac46d6c0a8b11c6
niharikav27 <neerjav80@gmail.com>
2026-05-04 23:42:38 +0530

    brute force-LS

--- Binary Search/find_sqrt.cpp
+++ Binary Search/find_sqrt.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int sqrt(int n){
     int ans = 1;
-    for(int i = 0 ; i<=n ; i++){
+    for(int i = 1 ; i<=n ; i++){
         if((i*i) <= n){
             ans = i;
         }else{
