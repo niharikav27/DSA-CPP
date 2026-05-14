8dc23716cc21a316b50373e3be4920010fff9241
niharikav27 <neerjav80@gmail.com>
2026-05-14 20:46:00 +0530

    brute force

--- Binary Search/aggressive_cows.cpp
+++ Binary Search/aggressive_cows.cpp
@@ -3,7 +3,7 @@ using namespace std;
 bool canWePlace(int n,vector<int>&arr,int dist ,int cows){
     int cntcows = 1;
     int last = arr[0];
-    for(int i = 0 ; i<n ; i++){
+    for(int i = 1 ; i<n ; i++){
         if(arr[i] - last >= dist){
             cntcows++;
             last = arr[i];
