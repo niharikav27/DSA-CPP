00f2f8d724b4c1942366af31a4024e490781abe1
niharikav27 <neerjav80@gmail.com>
2026-05-31 10:29:06 +0530

    optimal solution

---
+++ String/reverse_a_string1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseString(vector<char>& s){
+    int n = s.size();
+    int left = 0;
+    int right = n - 1;
+
+    while(left < right){
+        swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<char> s(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> s[i];
+    }
+
+    reverseString(s);
+
+    for(char ch : s){
+        cout << ch << "";
+    }
+
+    return 0;
+}
