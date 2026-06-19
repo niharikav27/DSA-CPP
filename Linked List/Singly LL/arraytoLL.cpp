534220dbedcefa12efd7a6748d92331d35777f3a
niharikav27 <neerjav80@gmail.com>
2026-06-19 10:58:41 +0530

    basic

---
+++ Linked List/Singly LL/arraytoLL.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+
+Node* arrayToLinkedList(int arr[], int size) {
+    if (size == 0) return nullptr;
+    Node* head = new Node(arr[0]);
+    Node* current = head;
+    for (int i = 1; i < size; i++) {
+        current->next = new Node(arr[i]);
+        current = current->next;
+    }
+
+    return head;
+}
+
+
