5a240cc73881ae516ea2d865805500f5be105b06
niharikav27 <neerjav80@gmail.com>
2026-06-21 10:39:46 +0530

    deletion in LL

---
+++ Linked List/Singly LL/delete_tail_of_LL.cpp
@@ -0,0 +1,26 @@
+/*
+Definition of singly linked list:
+class ListNode{
+  public:
+    int data;
+    ListNode *next;
+    ListNode() : data(0), next(nullptr) {}
+    ListNode(int x) : data(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : data(x), next(next) {}
+};
+*/
+
+class Solution {
+    public:
+        ListNode* deleteTail(ListNode* &head) {
+            if(head == NULL || head->next == NULL) return NULL;
+
+            ListNode* temp = head;
+            while(temp->next->next != NULL){
+                temp = temp -> next;
+            }
+            delete temp -> next;
+            temp -> next = nullptr;
+            return head;
+        }
+};
