e897b932c11a0d18f41801d893e33eeef9228f6b
niharikav27 <neerjav80@gmail.com>
2026-07-19 20:08:26 +0530

    brute force

---
+++ Linked List/MEDIUM/delete_middle_1.cpp
@@ -0,0 +1,41 @@
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head == NULL || head->next == NULL) return NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        fast = fast -> next -> next;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        ListNode* middle = slow -> next;
+        slow -> next = slow-> next-> next;
+        delete middle;
+        return head;
+    }
+};
