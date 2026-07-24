23cb55173ba324aba45ae8a37ae541918f08726b
niharikav27 <neerjav80@gmail.com>
2026-07-24 18:04:44 +0530

    optimal solution

---
+++ Linked List/Doubly LL/delete_all_occurrence_of_key.cpp
@@ -0,0 +1,51 @@
+/*
+Definition of doubly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode *prev;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1, ListNode *next1, ListNode *prev1)
+    {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode * deleteAllOccurrences(ListNode* head, int target) {
+        ListNode* temp = head;
+        while(temp != NULL){
+            if(temp -> val == target){
+                if(temp == head){
+                    head = head -> next;
+                }
+                ListNode* nextNode = temp -> next;
+                ListNode* prevNode = temp -> prev;
+                if(nextNode) nextNode -> prev = prevNode;
+                if(prevNode) prevNode -> next = nextNode;
+                delete temp; 
+                temp = nextNode;
+            }
+            else{
+                temp = temp -> next;
+            }
+        }
+        return head;
+    }
+};
